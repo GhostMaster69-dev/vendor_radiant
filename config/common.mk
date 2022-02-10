b572ffce235abc0534d2bc99d051bc2a50dce440
jhenrique09 <jhsv09@gmail.com>
2022-02-10 11:25:28 -0300

    vendor: Bring Back FaceUnlock
    Change-Id: I0beb2311a7fa469182b71c54f0773aac36b53354
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -200,4 +200,15 @@ $(call inherit-product, vendor/radiant/config/packages.mk)
 $(call inherit-product, vendor/gms/products/gms.mk)
 $(call inherit-product, vendor/radiant/config/bootanimation.mk)
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
